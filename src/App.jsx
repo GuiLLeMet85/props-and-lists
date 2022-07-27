1fd5baf9987dd3cb282c730535f6f26f5ee27de0
Guillem B <guillembaracco@gmail.com>
2022-07-27 08:31:49 +0200

    button part

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
+import React, { useState } from 'react';
 import './App.css';
-import React from 'react';
 import animalData from './data.json';
 import Card from './components/Card';
 
+
 function App() {
+
+  const  [animals, setAnimals] = useState(animalData)
+
+  const handleSortByLife = () => {
+    const orderThings = [...animals].sort((a, b) => a.lifeExpectancy - b.lifeExpectancy)
+    setAnimals(orderThings);
+  }
 
   return (
     <div className="App">
       <h1>Adopt me plz 🐾</h1>
-      {/* Some code goes here */}
+      {animals.map(elem => {
+        <button className="action-btn" onClick={handleSortByLife}>Sort by life</button>
+        {animals.map(elem => {
+        return <Card key={elem._id} animal={elem}/>
+      })}
     </div>
   );
 }
 
+
+   
 export default App;
+
